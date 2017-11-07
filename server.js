b749e84bb2decd9236da8eeeb6c8a10de326ccf1
William J Reid <willjreid@gmail.com>
2017-11-07 11:54:15 -0800

    commiting to deployment branch

--- server.js
+++ server.js
@@ -3,15 +3,12 @@
 const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
+const app = express();
+const cors = require('cors');
+
 // const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
-// const app = express();
 const conString = 'postgres://localhost:5432';
-// const client = new pg.Client(conString);
-// client.connect();
-// client.on('error', err => {
-//   console.error(err);
-// });
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
