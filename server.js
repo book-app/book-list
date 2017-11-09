f75df6e7788a42027e6bf4e44c8b15939fc6199a
William J Reid <willjreid@gmail.com>
2017-11-08 17:14:49 -0800

    updates

--- server.js
+++ server.js
@@ -27,15 +27,28 @@ app.use(express.static('./'));
 
 app.get('/test', (req, res) => res.send('Testing 1, 2, 3'));
 
-app.get('/api/vi/books', (req, res) => {
+app.get('/api/v1/books', (req, res) => {
   client.query(`SELECT book_id, title, author, image_url, isbn FROM books;`)
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
+
+app.get('/api/v1/books/:id', (req, res) => {
+  client.query(`SELECT * FROM books WHERE book_id=${req.params.id};`)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+app.post('/api/v1/books', bodyParser, (req, res) => {
+  let {title, author, isbn, image_url, description} = req.body; // destructuring your object
+  cliend.query(`INSERT INTO books(title, author, isbn, image_url, description) VALEUS($1, $2, $3, $4, $5)`,
+  [title, author, isbn, image_url, description]
+)
+    .then(results => res.sendStatus(201))
+    .catch(console.error);
+});
+
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
-
-
-
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 
